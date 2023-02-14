37a3c86ea6b1ff1b62be0338398f8d226046d023
Metin Güler <eng.metin@gmail.com>
2023-02-14 19:14:42 +0100

    add author name to post

--- RedditExplorer/Views/Post/PostHeaderView.swift
+++ RedditExplorer/Views/Post/PostHeaderView.swift
@@ -12,7 +12,11 @@ struct PostHeaderView: View {
     
     var body: some View {
         HStack {
-            Text(post.subreddit_name_prefixed)
+            HStack(spacing: 2) {
+                Text(post.subreddit_name_prefixed)
+                Text("·")
+                Text(post.author)
+            }
             Text(timeSince(post.created_utc))
         }.font(.caption)
     }
@@ -20,7 +24,7 @@ struct PostHeaderView: View {
 
 struct PostHeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        PostHeaderView(post: Post(id: "1", title: "Post Title", name: "Author 1", selftext: "", selftext_html: nil, thumbnail: "", is_reddit_media_domain: false, domain: "reddit.com", url: "", author: "", subreddit: "", subreddit_name_prefixed: "r/tifu", ups: 132, upvote_ratio: 0.87, num_comments: 0, stickied: false, created_utc: 0, preview: nil, is_video: false, link_flair_text: nil, is_original_content: false, spoiler: false, replies: nil, media: nil))
+        PostHeaderView(post: Post(id: "1", title: "Post Title", name: "t1_j8hutlf", selftext: "", selftext_html: nil, thumbnail: "", is_reddit_media_domain: false, domain: "reddit.com", url: "", author: "TheAuthor", subreddit: "", subreddit_name_prefixed: "r/tifu", ups: 132, upvote_ratio: 0.87, num_comments: 0, stickied: false, created_utc: 1676389720, preview: nil, is_video: false, link_flair_text: nil, is_original_content: false, spoiler: false, replies: nil, media: nil))
             .previewLayout(.sizeThatFits)
     }
 }
