e8f917df1f2d98272cf2a1fcbb865f5abd834057
Metin Güler <eng.metin@gmail.com>
2024-06-22 22:44:44 +0200

    test fix target

--- RedditExplorerTests/RedditExplorerTests.swift
+++ RedditExplorerTests/RedditExplorerTests.swift
@@ -2,13 +2,13 @@
 //  RedditExplorerTests.swift
 //  RedditExplorerTests
 //
-//  Created by Metin Güler on 19.03.22.
+//  Created by Metin Güler on 22.06.24.
 //
 
 import XCTest
-@testable import RedditExplorer
+@testable import RdD
 
-class RedditExplorerTests: XCTestCase {
+final class RedditExplorerTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -19,13 +19,11 @@ class RedditExplorerTests: XCTestCase {
     }
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
